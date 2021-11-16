91aeeba817768d924d3c5ff9939a12510d9878b7
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-16 12:09:58 +0400

    header + style

--- index.php
+++ index.php
@@ -7,12 +7,50 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS -->
     <link rel="stylesheet" href="css/style.css">
+    <!-- FONTS -->
+
+    <!-- text -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Karla:wght@200;300;400;500&display=swap" rel="stylesheet">
+
+    <!-- title -->
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Code+Latin:wght@500;600;700&display=swap" rel="stylesheet">
+
+    <!-- ICON -->
+    <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">
+
     <title>DISTRIB PC</title>
 </head>
 
 <body>
 
+    <header>
+        <nav class="main_navbar">
+            <div class="logo">
+                <a href="#">Distrib pc</a>
+            </div>
+            <ul class="menu">
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Nos produits</a></li>
+                <li><a href="#">Presentation</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <div class="search">
+                <form>
+                    <input type="search" name="search">
+                    <button>
+                        <span class="material-icons">
+                            search
+                        </span>
+                    </button>
+                </form>
+            </div>
+        </nav>
+        <div class="home">
 
+        </div>
+    </header>
 
     <script src='js/main.js'></script>
 </body>
