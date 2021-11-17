bd7a81552707eacf1ffc65a4131ef9c1ff3ca0d3
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-17 19:07:01 +0400

    decomposition des différentes partie réutilisable du code

---
+++ component/footer.php
@@ -0,0 +1,29 @@
+<footer>
+    <div class="container">
+        <div class="up">
+            <div class="logo">
+                <a href="#">Distrib pc</a>
+            </div>
+            <ul class="link">
+                <p>Navigation rapide</p>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Nos produits</a></li>
+                <li><a href="#">Presentation</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <ul class="legal">
+                <p>Légal</p>
+                <li><a href="#">Mentions légales</a></li>
+                <li><a href="#">Politique de confidentialités</a></li>
+            </ul>
+        </div>
+        <hr>
+        <div class="down">
+            <p>Copyright © 2021 HOUDJA. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
+    <script src='js/main.js'></script>
+</body>
+
+</html>
