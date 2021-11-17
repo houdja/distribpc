47861bf4b436e05b596cba560e300f8fe9f8cb38
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-17 19:04:19 +0400

    creation de la page de présentation

---
+++ presentation.php
@@ -0,0 +1,18 @@
+<?php
+    require_once('component/header.php');
+    require_once('component/navbar.php');
+?>
+
+<section class="presentation">
+    <div class="container">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+</section>
+
+<?php
+    require_once('component/footer.php');
+?>
