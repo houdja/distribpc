c9d78f94704f6bd03f8f6c51c9cb2502bddb9f4d
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-18 15:20:41 +0400

    modification accueil

--- index.php
+++ index.php
@@ -1,231 +1,93 @@
 <?php
-    require_once('component/header.php');
+require_once('component/header.php');
 ?>
 
-    <header>
-        <?php
-            require_once('component/navbar.php');
-        ?>
-        <div class="home">
-            <div class="home_text">
-                <h1>Distrib pc a votre service</h1>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium, consequuntur nesciunt hic aperiam maxime error repellat voluptatum reiciendis repudiandae, necessitatibus vel maiores delectus enim qui voluptatibus, reprehenderit ad animi ea.</p>
-                <button>
-                    <a href="#">Nos produits</a>
-                </button>
-            </div>
+<header>
+    <?php
+    require_once('component/navbar.php');
+    ?>
+    <div class="home">
+        <div class="home_text">
+            <h1>Distrib pc a votre service</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium, consequuntur nesciunt hic aperiam maxime error repellat voluptatum reiciendis repudiandae, necessitatibus vel maiores delectus enim qui voluptatibus, reprehenderit ad animi ea.</p>
+            <button>
+                <a href="#">Nos produits</a>
+            </button>
         </div>
-    </header>
+    </div>
+</header>
 
-    <section class="ventes">
-        <div class="container">
-            <h1>MEILLEURS VENTES</h1>
-            <div class="contain_card">
-                <div class="card">
-                    <img src="img/ecran6.jpg" alt="ECRAN" class='card_pic'>
-                    <div class="card_text">
-                        <p>Nom du produit</p>
-                        <p class='descr'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-                        <p>1090 €</p>
-                        <button>Plus de détails</button>
-                    </div>
-                </div>
-                <div class="card">
-                <img src="img/ecran6.jpg" alt="ECRAN" class='card_pic'>
-                    <div class="card_text">
-                        <p>Nom du produit</p>
-                        <p class='descr'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-                        <p>1090 €</p>
-                        <button>Plus de détails</button>
-                    </div>
-                </div>
-                <div class="card">
-                <img src="img/ecran6.jpg" alt="ECRAN" class='card_pic'>
-                    <div class="card_text">
-                        <p>Nom du produit</p>
-                        <p class='descr'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-                        <p>1090 €</p>
-                        <button>Plus de détails</button>
-                    </div>
-                </div>
-                <div class="card">
-                <img src="img/ecran6.jpg" alt="ECRAN" class='card_pic'>
-                    <div class="card_text">
-                        <p>Nom du produit</p>
-                        <p class='descr'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-                        <p>1090 €</p>
-                        <button>Plus de détails</button>
-                    </div>
-                </div>
-                <div class="card">
-                <img src="img/ecran6.jpg" alt="ECRAN" class='card_pic'>
-                    <div class="card_text">
-                        <p>Nom du produit</p>
-                        <p class='descr'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-                        <p>1090 €</p>
-                        <button>Plus de détails</button>
-                    </div>
-                </div>
-                <div class="card">
-                <img src="img/ecran6.jpg" alt="ECRAN" class='card_pic'>
-                    <div class="card_text">
-                        <p>Nom du produit</p>
-                        <p class='descr'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-                        <p>1090 €</p>
-                        <button>Plus de détails</button>
-                    </div>
-                </div>
-                <div class="card">
-                <img src="img/ecran6.jpg" alt="ECRAN" class='card_pic'>
-                    <div class="card_text">
-                        <p>Nom du produit</p>
-                        <p class='descr'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-                        <p>1090 €</p>
-                        <button>Plus de détails</button>
-                    </div>
-                </div>
-                <div class="card">
-                <img src="img/ecran6.jpg" alt="ECRAN" class='card_pic'>
-                    <div class="card_text">
-                        <p>Nom du produit</p>
-                        <p class='descr'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-                        <p>1090 €</p>
-                        <button>Plus de détails</button>
-                    </div>
-                </div>
-            </div>
+<section class="ventes">
+    <div class="container">
+        <h1>MEILLEURS VENTES</h1>
+        <div class="contain_card">
+            <?php
+            require_once('C:\xampp\htdocs\distribpc\modules\affiche_meilleure_ventes\affiche.php');
+            ?>
         </div>
-    </section>
+    </div>
+</section>
 
-    <section class="categorie">
-        <div class="container">
-            <div class="ordi_portable">
-                <a href="#">ORDI FIXES</a>
-            </div>
-            <div class="carte_graphique">
-                <a href="#">CARTE GRAPHIQUE</a>
-            </div>
-            <div class="processeur">
-                <a href="#">processeur</a>
-            </div>
-            <div class="carte_mere">
-                <a href="#">carte mere</a>
-            </div>
+<section class="categorie">
+    <div class="container">
+        <div class="ordi_portable">
+            <a href="#">ORDI FIXES</a>
         </div>
-    </section>
+        <div class="carte_graphique">
+            <a href="#">CARTE GRAPHIQUE</a>
+        </div>
+        <div class="processeur">
+            <a href="#">processeur</a>
+        </div>
+        <div class="carte_mere">
+            <a href="#">carte mere</a>
+        </div>
+    </div>
+</section>
 
-    <section class="nouveau">
-        <div class="container">
-            <h1>NOUVEAU PRODUITS</h1>
-            <div class="contain_card">
-                <div class="card">
-                <img src="img/ecran6.jpg" alt="ECRAN" class='card_pic'>
-                    <div class="card_text">
-                        <p>Nom du produit</p>
-                        <p class='descr'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-                        <p>1090 €</p>
-                        <button>Plus de détails</button>
-                    </div>
-                </div>
-                <div class="card">
-                <img src="img/ecran6.jpg" alt="ECRAN" class='card_pic'>
-                    <div class="card_text">
-                        <p>Nom du produit</p>
-                        <p class='descr'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-                        <p>1090 €</p>
-                        <button>Plus de détails</button>
-                    </div>
-                </div>
-                <div class="card">
-                <img src="img/ecran6.jpg" alt="ECRAN" class='card_pic'>
-                    <div class="card_text">
-                        <p>Nom du produit</p>
-                        <p class='descr'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-                        <p>1090 €</p>
-                        <button>Plus de détails</button>
-                    </div>
-                </div>
-                <div class="card">
-                <img src="img/ecran6.jpg" alt="ECRAN" class='card_pic'>
-                    <div class="card_text">
-                        <p>Nom du produit</p>
-                        <p class='descr'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-                        <p>1090 €</p>
-                        <button>Plus de détails</button>
-                    </div>
-                </div>
-                <div class="card">
-                <img src="img/ecran6.jpg" alt="ECRAN" class='card_pic'>
-                    <div class="card_text">
-                        <p>Nom du produit</p>
-                        <p class='descr'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-                        <p>1090 €</p>
-                        <button>Plus de détails</button>
-                    </div>
-                </div>
-                <div class="card">
-                <img src="img/ecran6.jpg" alt="ECRAN" class='card_pic'>
-                    <div class="card_text">
-                        <p>Nom du produit</p>
-                        <p class='descr'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-                        <p>1090 €</p>
-                        <button>Plus de détails</button>
-                    </div>
-                </div>
-                <div class="card">
-                <img src="img/ecran6.jpg" alt="ECRAN" class='card_pic'>
-                    <div class="card_text">
-                        <p>Nom du produit</p>
-                        <p class='descr'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-                        <p>1090 €</p>
-                        <button>Plus de détails</button>
-                    </div>
-                </div>
-                <div class="card">
-                <img src="img/ecran6.jpg" alt="ECRAN" class='card_pic'>
-                    <div class="card_text">
-                        <p>Nom du produit</p>
-                        <p class='descr'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-                        <p>1090 €</p>
-                        <button>Plus de détails</button>
-                    </div>
-                </div>
-            </div>
+<section class="nouveau">
+    <div class="container">
+        <h1>NOUVEAU PRODUITS</h1>
+        <div class="contain_card">
+            <?php
+            require_once('C:\xampp\htdocs\distribpc\modules\affiche_dernier_produit\affiche.php');
+            ?>
         </div>
-    </section>
+    </div>
+</section>
 
-    <section class="tag">
-        <div class="container">
-            <h1>Produits recherchés</h1>
-            <div class="produit">
-                <a href="#">AMD Ryzen 5 5600X</a>
-                <a href="#">Intel Core i7 12700K</a>
-                <a href="#">AMD Ryzen 7 3700X</a>
-            </div>
-            <div class="produit">
-                <a href="#">Fox Spirit PGM270</a>
-                <a href="#">MSI MAG ARTYMIS 242C</a>
-                <a href="#">Fox Spirit FH245</a>
-            </div>
-            <div class="produit">
-                <a href="#">Asus Radeon RX 6900 XT</a>
-                <a href="#">Asus GeForce RTX 3060</a>
-                <a href="#">Gigabyte Radeon RX 6600</a>
-            </div>
+<section class="tag">
+    <div class="container">
+        <h1>Produits recherchés</h1>
+        <div class="produit">
+            <a href="#">AMD Ryzen 5 5600X</a>
+            <a href="#">Intel Core i7 12700K</a>
+            <a href="#">AMD Ryzen 7 3700X</a>
         </div>
-    </section>
+        <div class="produit">
+            <a href="#">Fox Spirit PGM270</a>
+            <a href="#">MSI MAG ARTYMIS 242C</a>
+            <a href="#">Fox Spirit FH245</a>
+        </div>
+        <div class="produit">
+            <a href="#">Asus Radeon RX 6900 XT</a>
+            <a href="#">Asus GeForce RTX 3060</a>
+            <a href="#">Gigabyte Radeon RX 6600</a>
+        </div>
+    </div>
+</section>
 
-    <section class="partenaires">
-        <div class="contain_partenaires">
-            <img src="img/partner1.png">
-            <img src="img/partner2.png">
-            <img src="img/partner3.png">
-            <img src="img/partner4.png">
-            <img src="img/partner5.png">
-            <img src="img/partner6.png">
-        </div>
-    </section>
+<section class="partenaires">
+    <div class="contain_partenaires">
+        <img src="img/partner1.png">
+        <img src="img/partner2.png">
+        <img src="img/partner3.png">
+        <img src="img/partner4.png">
+        <img src="img/partner5.png">
+        <img src="img/partner6.png">
+    </div>
+</section>
 
 <?php
-    require_once('component/footer.php');
+require_once('component/footer.php');
 ?>
