28c804cd1449b4a1faea72e55819b481a1eb0511
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-18 15:20:54 +0400

    page presentation

--- presentation.php
+++ presentation.php
@@ -1,18 +1,55 @@
 <?php
-    require_once('component/header.php');
-    require_once('component/navbar.php');
+require_once('component/header.php');
+require_once('component/navbar.php');
 ?>
 
 <section class="presentation">
     <div class="container">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div class='text'>
+            <h1>Qui sommes nous ?</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque est provident eos, reprehenderit corporis aspernatur dolores, earum sunt porro enim ducimus. Nemo tenetur praesentium ad, necessitatibus quos distinctio veniam sint.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum sed animi tempore expedita officia architecto fuga modi distinctio, officiis mollitia ab eaque, numquam iste et voluptatem voluptatibus sit voluptates! Quae?
+            </p>
+        </div>
+        <div class='pic'></div>
+        <div class='contain_card'>
+            <div class="card">
+                <img src="img/team1.jpg" alt="People" class="card_pic">
+                <div class="card_text">
+                    <p>NOM</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid repudiandae corrupti nobis? Nihil natus enim eveniet</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="img/team2.jpg" alt="People" class="card_pic">
+                <div class="card_text">
+                    <p>NOM</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid repudiandae corrupti nobis? Nihil natus enim eveniet</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="img/team3.jpg" alt="People" class="card_pic">
+                <div class="card_text">
+                    <p>NOM</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid repudiandae corrupti nobis? Nihil natus enim eveniet</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="img/team4.jpg" alt="People" class="card_pic">
+                <div class="card_text">
+                    <p>NOM</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid repudiandae corrupti nobis? Nihil natus enim eveniet</p>
+                </div>
+            </div>
+        </div>
+        <div class='pic'></div>
+        <div class='text'>
+            <h1>Que faisant nous ?</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque est provident eos, reprehenderit corporis aspernatur dolores, earum sunt porro enim ducimus. Nemo tenetur praesentium ad, necessitatibus quos distinctio veniam sint.Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum sed animi tempore expedita officia architecto fuga modi distinctio, officiis mollitia ab eaque, numquam iste et voluptatem voluptatibus sit voluptates! Quae?
+            </p>
+        </div>
     </div>
 </section>
 
 <?php
-    require_once('component/footer.php');
+require_once('component/footer.php');
 ?>
