d8b81d93a18684afb1da5cb8fbf15ef1ce0f0617
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-18 12:43:23 +0400

    requete + affiche dernier produit

---
+++ modules/affiche_dernier_produit/affiche.php
@@ -0,0 +1,15 @@
+<?php
+include('C:\xampp\htdocs\distribpc\modules\affiche_dernier_produit\requete_dernier_produit.php');
+?>
+
+<?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) : ?>
+    <div class="card">
+        <img src="img/<?php echo htmlspecialchars($row['image_produit']); ?>" alt="<?php echo htmlspecialchars($row['nom_produit']); ?>" class='card_pic'>
+        <div class="card_text">
+            <p><?php echo htmlspecialchars($row['nom_produit']); ?></p>
+            <p class='descr'><?php echo htmlspecialchars($row['description_courte']); ?></p>
+            <p><?php echo htmlspecialchars($row['prix']); ?> €</p>
+            <button><a href="modules/detailproduit/detail_produit.php?id_produit=<?php echo htmlspecialchars($row['id_produit']); ?>">Plus de details</a></button>
+        </div>
+    </div>
+<?php endwhile; ?>
