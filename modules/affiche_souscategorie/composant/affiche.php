e886b9a72f756baa31295e89578ef3eff611930e
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-18 12:45:50 +0400

    requete + affiche les liens pour les ordinateur

---
+++ modules/affiche_souscategorie/composant/affiche.php
@@ -0,0 +1,10 @@
+<?php
+include('C:/xampp/htdocs/distribpc/modules/affiche_souscategorie/composant/requete_composant.php');
+?>
+
+<ul>
+    <a href="#">Composants</a>
+    <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) : ?>
+        <li><a href="#"> <?php echo htmlspecialchars($row['nom_souscategorie']); ?></a></li>
+    <?php endwhile; ?>
+</ul>
