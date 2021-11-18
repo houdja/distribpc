6f680996df94552692d9dfe9feffa74a520c268b
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-18 12:47:55 +0400

    requete + affiche lien periphérque

---
+++ modules/affiche_souscategorie/peripherique/affiche.php
@@ -0,0 +1,10 @@
+<?php
+include('C:/xampp/htdocs/distribpc/modules/affiche_souscategorie/peripherique/requete_peripherique.php');
+?>
+
+<ul>
+    <a href="#">Périphériques</a>
+    <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) : ?>
+        <li><a href="#"> <?php echo htmlspecialchars($row['nom_souscategorie']); ?></a></li>
+    <?php endwhile; ?>
+</ul>
