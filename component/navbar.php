d6f6c1d308ad3e4e693ff7d470ec158ebe73f48a
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-18 15:20:28 +0400

    ajout lien

--- component/navbar.php
+++ component/navbar.php
@@ -1,53 +1,38 @@
 <nav class="main_navbar">
-            <div class="logo">
-                <a href="index.php">Distrib pc</a>
+    <div class="logo">
+        <a href="http://localhost/distribpc/index.php">Distrib pc</a>
+    </div>
+    <ul class="menu">
+        <li><a href="http://localhost/distribpc/index.php">Accueil</a></li>
+        <li class='has_children'><a href="http://localhost/distribpc/produit.php">Nos produits</a>
+            <div class="submenu">
+                <div class="container">
+                    <ul>
+                        <?php require_once('C:\xampp\htdocs\distribpc\modules\affiche_souscategorie\ordinateur\affiche.php')
+                        ?>
+                    </ul>
+                    <ul>
+                        <?php require_once('C:\xampp\htdocs\distribpc\modules\affiche_souscategorie\peripherique\affiche.php')
+                        ?>
+                    </ul>
+                    <ul>
+                        <?php require_once('C:\xampp\htdocs\distribpc\modules\affiche_souscategorie\composant\affiche.php')
+                        ?>
+                    </ul>
+                </div>
             </div>
-            <ul class="menu">
-                <li><a href="index.php">Accueil</a></li>
-                <li class='has_children'><a href="#">Nos produits</a>
-                    <div class="submenu">
-                        <div class="container">
-                            <ul>
-                                <a href="#">Ordinateurs</a>
-                                <li><a href="#">Ordinateur fixes</a></li>
-                                <li><a href="#">Ordinateur portables</a></li>
-                            </ul>
-                            <ul>
-                                <a href="#">Périphériques</a>
-                                <li><a href="#">Ecran</a></li>
-                                <li><a href="#">Casque</a></li>
-                                <li><a href="#">Clavier</a></li>
-                                <li><a href="#">Souris</a></li>
-                                <li><a href="#">Micro</a></li>
-                                <li><a href="#">Enceintes</a></li>
-                                <li><a href="#">Stackage externes</a></li>
-                                <li><a href="#">Manettes</a></li>
-                            </ul>
-                            <ul>
-                                <a href="#">Composants</a>
-                                <li><a href="#">Procésseurs</a></li>
-                                <li><a href="#">Carte mere</a></li>
-                                <li><a href="#">Carte graphique</a></li>
-                                <li><a href="#">Alimentation</a></li>
-                                <li><a href="#">Refroidissent</a></li>
-                                <li><a href="#">Méroires</a></li>
-                                <li><a href="#">Boitier</a></li>
-                                <li><a href="#">Carte réseau</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </li>
-                <li><a href="presentation.php">Presentation</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-            <div class="search">
-                <form>
-                    <input type="search" name="search">
-                    <button>
-                        <span class="material-icons">
-                            search
-                        </span>
-                    </button>
-                </form>
-            </div>
-        </nav>
+        </li>
+        <li><a href="http://localhost/distribpc/presentation.php">Presentation</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+    <div class="search">
+        <form>
+            <input type="search" name="search">
+            <button>
+                <span class="material-icons">
+                    search
+                </span>
+            </button>
+        </form>
+    </div>
+</nav>
