db7a82b2de61dab3d06ffd8959dbeef0d41d4091
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-18 12:47:04 +0400

    requete + affiche lien ordinateur

---
+++ modules/affiche_souscategorie/ordinateur/requete_ordinateur.php
@@ -0,0 +1,16 @@
+<?php
+
+include('C:/xampp/htdocs/distribpc/modules/connexiondb/connexiondb.php');
+
+$sql = " SELECT souscategorie.nom_souscategorie FROM souscategorie WHERE souscategorie.id_categorie = (SELECT categorie.id_categorie FROM categorie WHERE categorie.nom_categorie = 'Ordinateurs') ";
+
+try {
+
+    //requete
+    $stmt = $db->query($sql);
+
+    if ($stmt === false) {
+        die("Erreur");
+    }
+} catch (PDOException $e) {
+}
