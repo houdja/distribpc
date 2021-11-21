17b7105d225ee045045d9487e7fc1cbe53b928a9
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-21 19:27:20 +0400

    requete + affiche tous les periphériques

---
+++ modules/affiche_peripherique/affiche_peripherique.php
@@ -0,0 +1,48 @@
+<?php
+include('C:/xampp/htdocs/distribpc/modules/affiche_peripherique/requete_peripherique.php');
+?>
+
+<?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) : ?>
+    <div class="card_produit">
+        <img src="http://localhost/distribpc/img/<?php echo htmlspecialchars($row['image_produit']); ?>" alt="<?php echo htmlspecialchars($row['nom_produit']); ?>" class="produit_pic">
+        <div class="info_produit">
+            <div class="left">
+                <p><b><?php echo htmlspecialchars($row['marque']); ?></b></p>
+                <p><b><?php echo htmlspecialchars($row['nom_produit']); ?></b></p>
+                <p class="produit_desc"><?php echo htmlspecialchars($row['description_courte']); ?>...</p>
+                <button><a href="modules/detailproduit/detail_produit.php?id_produit=<?php echo htmlspecialchars($row['id_produit']); ?>">Plus de details</a></button>
+            </div>
+            <div class="right">
+                <p><?php echo htmlspecialchars($row['prix']); ?> €</p>
+                <?php
+                if (htmlspecialchars($row['disponible']) == 'disponible') { ?>
+
+                    <p class='disponible'>Disponible</p>
+
+                <?php } else { ?>
+
+                    <p class='pas_dispo'>Pas dispo</p>
+
+                <?php } ?>
+
+            </div>
+        </div>
+    </div>
+<?php endwhile; ?>
+
+<?php
+echo '<p align="center" class="pagination"> '; //Pour l'affichage, on centre la liste des pages
+for ($i = 1; $i <= $nombreDePages; $i++) //On fait notre boucle
+{
+    //On va faire notre condition
+    if ($i == $pageActuelle) //Si il s'agit de la page actuelle...
+    {
+        echo ' [ ' . $i . ' ] ';
+    } else //Sinon...
+    {
+        echo ' <a href="peripherique.php?page=' . $i . '">' . $i . '</a> ';
+    }
+}
+echo '</p>';
+
+?>
---
+++ modules/affiche_peripherique/requete_peripherique.php
@@ -0,0 +1,17 @@
+<?php
+include('C:/xampp/htdocs/distribpc/modules/connexiondb/connexiondb.php');
+include("modules/pagination/compteur.php");
+
+
+$sql = " SELECT * FROM produits WHERE produits.id_categorie = (SELECT categorie.id_categorie FROM categorie WHERE categorie.nom_categorie = 'Périphériques') LIMIT " . $premiereEntree . ", " . $messagesParPage . " ";
+
+try {
+
+    //requete
+    $stmt = $db->query($sql);
+
+    if ($stmt === false) {
+        die("Erreur");
+    }
+} catch (PDOException $e) {
+}
