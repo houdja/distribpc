bc943769794dd06ef04be7f0c9d5704c5f34b03a
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-22 18:18:37 +0400

    modif lien

--- modules/affiche_souscategorie/peripherique/affiche.php
+++ modules/affiche_souscategorie/peripherique/affiche.php
@@ -3,7 +3,7 @@ include('C:/xampp/htdocs/distribpc/modules/affiche_souscategorie/peripherique/re
 ?>
 
 <ul>
-    <a href="#">Périphériques</a>
+    <a href="http://localhost/distribpc/peripherique.php">Périphériques</a>
     <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) : ?>
         <li><a href="#"> <?php echo htmlspecialchars($row['nom_souscategorie']); ?></a></li>
     <?php endwhile; ?>
