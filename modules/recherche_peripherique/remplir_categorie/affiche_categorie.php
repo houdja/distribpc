8a5d5ec9209c269178b38115ce85c287aeb74d5f
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-22 21:17:11 +0400

    affiche les categorie

---
+++ modules/recherche_peripherique/remplir_categorie/affiche_categorie.php
@@ -0,0 +1,9 @@
+<?php
+include('C:/xampp/htdocs/distribpc/modules/recherche_peripherique/remplir_categorie/remplir_categorie.php');
+?>
+
+<select name="categorie">
+        <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) : ?>
+                <option value="<?php echo htmlspecialchars($row['id_souscategorie']); ?>"><?php echo htmlspecialchars($row['nom_souscategorie']); ?></option>
+        <?php endwhile; ?>
+</select>
