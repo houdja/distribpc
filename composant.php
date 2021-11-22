a63aa8802456cda856bd600781fb1e06f85a88ee
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-22 18:14:27 +0400

    page categorie composant

--- composant.php
+++ composant.php
@@ -8,26 +8,27 @@ require_once('component/navbar.php');
     <div class="container">
         <div class="filtre">
             <h2>Affinez votre recherche</h2>
-            <form class="par_nom">
+            <form class="par_nom" action='modules/recherche_composant/traitement_composant.php' method='POST'>
                 <div>
-                    <label>Recherche par nom</label>
-                    <input type="text" name="nom_produit" placeholder="Nom du produit" required>
+                    <label>Marque</label>
+                    <select name="marque">
+                        <option value="Asus">Asus</option>
+                        <option value="Ballistix">Ballistix</option>
+                        <option value="Abkoncore">Abkoncore</option>
+                    </select>
                 </div>
 
                 <div>
                     <label>Prix</label>
-                    <input type="range" min='0' max='2000' step='10' value="0" class='filtre_prix'>
+                    <input type="range" name='prix' min='0' max='2000' step='10' value="0" class='filtre_prix'>
                     <p class='affiche_prix'>0€</p>
                 </div>
                 <div>
                     <label>Categorie</label>
                     <select name="categorie">
-                        <option value="0">0</option>
-                        <option value="0">1</option>
-                        <option value="0">2</option>
-                        <option value="0">3</option>
-                        <option value="0">4</option>
-                        <option value="0">5</option>
+                        <option value="9">RAM</option>
+                        <option value="12">Carte graphique</option>
+                        <option value="16">Boitier</option>
                     </select>
                 </div>
 
