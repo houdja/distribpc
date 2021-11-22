0479b5c543ec19024b0dad55e4165700d34decf7
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-22 21:18:53 +0400

    modification de l'emplacement dont la suppression

--- modules/recherche_tout_produit/affiche_categorie.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('C:/xampp/htdocs/distribpc/modules/recherche_tout_produit/remplir_categorie.php');
-?>
-
-<select name="categorie">
-<?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) : ?>
-        <option value="<?php echo htmlspecialchars($row['id_souscategorie']); ?>"><?php echo htmlspecialchars($row['nom_souscategorie']); ?></option>
-<?php endwhile; ?>
-</select>
--- modules/recherche_tout_produit/affiche_marque.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('C:/xampp/htdocs/distribpc/modules/recherche_tout_produit/remplir_marque.php');
-?>
-
-<select name="marque">
-<?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) : ?>
-        <option value="<?php echo htmlspecialchars($row['marque']); ?>"><?php echo htmlspecialchars($row['marque']); ?></option>
-<?php endwhile; ?>
-</select>
