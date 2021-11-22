9f3bea5ae2232c8504fcebb1927d7eef9e1bf21e
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-22 21:18:24 +0400

    requete pour afficher les marques

---
+++ modules/recherche_composant/remplir_marque/remplir_marque.php
@@ -0,0 +1,16 @@
+<?php
+
+include('C:/xampp/htdocs/distribpc/modules/connexiondb/connexiondb.php');
+
+$sql = " SELECT DISTINCT marque FROM produits WHERE produits.id_categorie = (SELECT categorie.id_categorie FROM categorie WHERE categorie.nom_categorie = 'Composants') ";
+
+try {
+
+    //requete
+    $stmt = $db->query($sql);
+
+    if ($stmt === false) {
+        die("Erreur");
+    }
+} catch (PDOException $e) {
+}
