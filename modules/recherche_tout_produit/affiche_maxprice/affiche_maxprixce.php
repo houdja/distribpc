82e8a2f6868362c4ce3d3f7366f1d0325f098335
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-22 21:15:16 +0400

    afficher le prix du produit le + cher selon la page

---
+++ modules/recherche_tout_produit/affiche_maxprice/affiche_maxprixce.php
@@ -0,0 +1,7 @@
+<?php
+include('C:/xampp/htdocs/distribpc/modules/recherche_tout_produit/affiche_maxprice/remplir_maxprice.php');
+?>
+
+<?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) : ?>
+        <input type="range" name='prix' min='0' max='<?php echo htmlspecialchars($row['prix'] + 1); ?>' step='10' value="0" class='filtre_prix'>
+<?php endwhile; ?>
