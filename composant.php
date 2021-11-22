c7ca9b138a99f0ab013631a3123865c2087d5d1b
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-22 21:08:04 +0400

    ajout affichage  maxprix, categorie,marque

--- composant.php
+++ composant.php
@@ -9,29 +9,26 @@ require_once('component/navbar.php');
         <div class="filtre">
             <h2>Affinez votre recherche</h2>
             <form class="par_nom" action='modules/recherche_composant/traitement_composant.php' method='POST'>
-                <div>
-                    <label>Marque</label>
-                    <select name="marque">
-                        <option value="Asus">Asus</option>
-                        <option value="Ballistix">Ballistix</option>
-                        <option value="Abkoncore">Abkoncore</option>
-                    </select>
-                </div>
 
                 <div>
                     <label>Prix</label>
-                    <input type="range" name='prix' min='0' max='2000' step='10' value="0" class='filtre_prix'>
+                    <?php
+                    require_once('C:/xampp/htdocs/distribpc/modules/recherche_composant/affiche_maxprice/affiche_maxprixce.php')
+                    ?>
                     <p class='affiche_prix'>0€</p>
                 </div>
                 <div>
                     <label>Categorie</label>
-                    <select name="categorie">
-                        <option value="9">RAM</option>
-                        <option value="12">Carte graphique</option>
-                        <option value="16">Boitier</option>
-                    </select>
+                    <?php
+                    require_once('C:/xampp/htdocs/distribpc/modules/recherche_composant/remplir_categorie/affiche_categorie.php')
+                    ?>
                 </div>
-
+                <div>
+                    <label>Marque</label>
+                    <?php
+                    require_once('C:/xampp/htdocs/distribpc/modules/recherche_composant/remplir_marque/affiche_marque.php')
+                    ?>
+                </div>
                 <button type="submit">Rechercher</button>
             </form>
         </div>
