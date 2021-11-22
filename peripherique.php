38d4c47636d2b9dfbd9f590a5a8d272a36d14ee4
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-22 18:15:03 +0400

    page categorie peripherique

--- peripherique.php
+++ peripherique.php
@@ -8,26 +8,23 @@ require_once('component/navbar.php');
     <div class="container">
         <div class="filtre">
             <h2>Affinez votre recherche</h2>
-            <form class="par_nom">
-                <div>
-                    <label>Recherche par nom</label>
-                    <input type="text" name="nom_produit" placeholder="Nom du produit" required>
-                </div>
-
+            <form class="par_nom" method='POST' action='modules/recherche_peripherique/traitement_peripherique.php'>
                 <div>
                     <label>Prix</label>
-                    <input type="range" min='0' max='2000' step='10' value="0" class='filtre_prix'>
+                    <input type="range" name='prix' min='0' max='2000' step='10' value="0" class='filtre_prix'>
                     <p class='affiche_prix'>0€</p>
                 </div>
                 <div>
+                <label>Marque</label>
+                <select name="marque">
+                    <option value="MSI">MSI</option>
+                    <option value="Samsung">Samsung</option>
+                </select>
+            </div>
+                <div>
                     <label>Categorie</label>
                     <select name="categorie">
-                        <option value="0">0</option>
-                        <option value="0">1</option>
-                        <option value="0">2</option>
-                        <option value="0">3</option>
-                        <option value="0">4</option>
-                        <option value="0">5</option>
+                        <option value="3">Ecran</option>
                     </select>
                 </div>
 
