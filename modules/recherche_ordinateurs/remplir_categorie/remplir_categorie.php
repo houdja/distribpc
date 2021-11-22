3bbd08fc64ace7917fcd7aa4494096d4da2c4ee3
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-22 21:17:37 +0400

    requete pour afficher les categories

---
+++ modules/recherche_ordinateurs/remplir_categorie/remplir_categorie.php
@@ -0,0 +1,19 @@
+<?php
+
+include('C:/xampp/htdocs/distribpc/modules/connexiondb/connexiondb.php');
+
+$sql = " SELECT DISTINCT souscategorie.id_souscategorie, souscategorie.nom_souscategorie FROM souscategorie 
+JOIN produits ON 
+( produits.id_souscategorie = souscategorie.id_souscategorie )
+WHERE produits.id_categorie = ( SELECT categorie.id_categorie FROM categorie WHERE categorie.nom_categorie ='Ordinateurs') ";
+
+try {
+
+    //requete
+    $stmt = $db->query($sql);
+
+    if ($stmt === false) {
+        die("Erreur");
+    }
+} catch (PDOException $e) {
+}
