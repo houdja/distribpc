f5f175217bab951f3e8f75032509e5da776b3026
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-22 18:23:56 +0400

    requete resultat filtre ordinateur

---
+++ modules/recherche_ordinateurs/requete.php
@@ -0,0 +1,38 @@
+<?php
+
+include('C:/xampp/htdocs/distribpc/modules/connexiondb/connexiondb.php');
+
+$categorie = $_POST['categorie'];
+$marque = $_POST['marque'];
+$prix = $_POST['prix'];
+
+$sql = " SELECT * FROM produits WHERE produits.id_categorie = (SELECT categorie.id_categorie FROM categorie WHERE categorie.nom_categorie = 'Ordinateurs') AND id_souscategorie = :categorie AND marque = :marque AND prix <= :prix ";
+
+try{
+
+$stmt = $db->prepare($sql);
+$stmt->bindParam('categorie', $categorie, PDO::PARAM_STR);
+$stmt->bindParam('marque', $marque, PDO::PARAM_STR);
+$stmt->bindParam('prix', $prix, PDO::PARAM_STR);
+$stmt->execute();
+$count = $stmt->rowCount();
+
+if ($count == 0) {
+
+    $msg = "Aucun résultat pour votre recherche";
+}
+
+
+
+
+
+
+if ($stmt === false) {
+  die("Erreur");
+}
+
+} catch (PDOException $e) {
+
+}
+
+?>
