62516654ef762c6e9d16ade3fac4801328ae8cca
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-25 15:34:00 +0400

    decomposition des parties réutilisables

---
+++ webadmin/component/aside.php
@@ -0,0 +1,9 @@
+<ul class="aside_menu">
+    <li class="<?php echo $_SERVER['REQUEST_URI'] == '/distribpc/webadmin/index.php' ?  'active' : '';  ?>"><a href="http://localhost/distribpc/webadmin/index.php"><img src="https://img.icons8.com/small/64/000000/home-page.png" /> Dashboard</a></li>
+
+    <li class=" <?php echo $_SERVER['REQUEST_URI'] == '/distribpc/webadmin/produit.php' ?  'active' : '';  ?>"><a href="http://localhost/distribpc/webadmin/produit.php"><img src="https://img.icons8.com/ios-glyphs/64/000000/activity-grid-2.png" /> Gestion produits</a></li>
+
+    <li><a href="#"><img src="https://img.icons8.com/ios-glyphs/64/000000/activity-grid-2.png" /> Gestion Categorie</a></li>
+    <li><a href="#"><img src="https://img.icons8.com/ios-glyphs/64/000000/activity-grid-2.png" /> Gestion SousCategorie</a></li>
+    <li><a href="#"><img src="https://img.icons8.com/small/64/000000/shutdown.png" />Déconnexion</a></li>
+</ul>
---
+++ webadmin/component/nav.php
@@ -0,0 +1,9 @@
+<nav>
+    <a href="http://localhost/distribpc/webadmin/index.php" class="logo">Distrib pc</a>
+    <div class="compte">
+        <a href="#"><img src="http://localhost/distribpc/webadmin/img/profile.jpg" alt="Profile picture"></a>
+        <?php
+        //echo '<h4>Bienvenue  ' . $_SESSION['sess_name'] . '</h4>';
+        ?>
+    </div>
+</nav>
