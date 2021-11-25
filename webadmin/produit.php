9f3f7d23faff03c421f4cb1298831f563574be75
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-25 15:32:49 +0400

    page produit

---
+++ webadmin/produit.php
@@ -0,0 +1,41 @@
+<?php
+require_once('C:\xampp\htdocs\distribpc\webadmin\component\header.php');
+?>
+
+<section class="dashboard">
+    <?php
+    require_once('C:/xampp/htdocs/distribpc/webadmin/component/nav.php') ?>
+    <aside>
+        <?php
+        require_once('C:/xampp/htdocs/distribpc/webadmin/component/aside.php') ?>
+    </aside>
+    <main>
+        <div class='produit'>
+            <div class="en_tete">
+                <h1>Liste des produits</h1>
+                <button><a href="http://localhost/distribpc/webadmin/modules/produits/ajouter/ajouter_produit.php"> Ajouter</a></button>
+            </div>
+            <table>
+                <tr class='titre'>
+                    <th>N° réference</th>
+                    <th>Categorie</th>
+                    <th>Sous categorie</th>
+                    <th>Nom</th>
+                    <th>Marque</th>
+                    <th>Prix</th>
+                    <th>Disponibilité</th>
+                    <th>Promo</th>
+                    <th>% promo</th>
+                    <th>Garantit</th>
+                    <th>Modifier</th>
+                    <th>Supprimer</th>
+                </tr>
+                <?php
+                require_once('C:\xampp\htdocs\distribpc\webadmin\modules\produits\lister\affiche_produit.php')
+                ?>
+        </div>
+    </main>
+</section>
+
+<?php
+require_once('C:/xampp/htdocs/distribpc/webadmin/component/footer.php') ?>
