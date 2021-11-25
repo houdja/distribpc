480e4af6ccb1fd4f3c072e0fd56231984865353c
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-25 15:32:31 +0400

    page d'accueil webadmin

---
+++ webadmin/index.php
@@ -0,0 +1,17 @@
+<?php
+require_once('C:/xampp/htdocs/distribpc/webadmin/component/header.php') ?>
+
+<section class="dashboard">
+    <?php
+    require_once('C:/xampp/htdocs/distribpc/webadmin/component/nav.php') ?>
+    <aside>
+        <?php
+        require_once('C:/xampp/htdocs/distribpc/webadmin/component/aside.php') ?>
+    </aside>
+    <main>
+        <div class="active"></div>
+    </main>
+</section>
+
+<?php
+require_once('C:/xampp/htdocs/distribpc/webadmin/component/footer.php') ?>
