cb0c7451a296ab49665070674797c77d987760af
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-28 12:16:53 +0400

    ajout des cartes sur l'index du dashboard

--- webadmin/index.php
+++ webadmin/index.php
@@ -9,7 +9,31 @@ require_once('C:/xampp/htdocs/distribpc/webadmin/component/header.php') ?>
         require_once('C:/xampp/htdocs/distribpc/webadmin/component/aside.php') ?>
     </aside>
     <main>
-        <div class="active"></div>
+        <div class="all_stats">
+            <h1>Statistiques</h1>
+            <div class="number_products">
+                <?php
+                require_once('C:/xampp/htdocs/distribpc/webadmin/modules/data_dashboard/all_produit/affiche.php')
+                ?>
+            </div>
+            <div class="contain_stats">
+                <?php
+                require_once('C:/xampp/htdocs/distribpc/webadmin/modules/data_dashboard/nombre_ordinateurs/affiche.php')
+                ?>
+                <?php
+                require_once('C:/xampp/htdocs/distribpc/webadmin/modules/data_dashboard/nombre_composants/affiche.php')
+                ?>
+                <?php
+                require_once('C:/xampp/htdocs/distribpc/webadmin/modules/data_dashboard/nombre_peripheriques/affiche.php')
+                ?>
+                <?php
+                require_once('C:/xampp/htdocs/distribpc/webadmin/modules/data_dashboard/best_seller/affiche.php')
+                ?>
+                <?php
+                require_once('C:/xampp/htdocs/distribpc/webadmin/modules/data_dashboard/rupture_stock/affiche.php')
+                ?>
+            </div>
+        </div>
     </main>
 </section>
 
