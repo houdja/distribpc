b32f366c562ff8c7cf490b6b92e30515191e29ee
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-28 12:24:19 +0400

    creation page + affichage du tableau correspondant

---
+++ webadmin/modules/data_dashboard/data_details/composant.php
@@ -0,0 +1,46 @@
+<?php
+require_once('C:/xampp/htdocs/distribpc/webadmin/component/header.php') ?>
+
+<section class="dashboard">
+    <?php
+    require_once('C:/xampp/htdocs/distribpc/webadmin/component/nav.php') ?>
+    <aside>
+        <?php
+        require_once('C:/xampp/htdocs/distribpc/webadmin/component/aside.php') ?>
+    </aside>
+    <main>
+        <div class="details">
+            <h1>Détails</h1>
+            <div class="contain_details">
+                <div class="info_categorie">
+                    <div class="active">
+                        <div class='produit'>
+                            <table>
+                                <tr class='titre'>
+                                    <th>N° réference</th>
+                                    <th>Categorie</th>
+                                    <th>Sous categorie</th>
+                                    <th>Nom</th>
+                                    <th>Marque</th>
+                                    <th>Prix</th>
+                                    <th>Disponibilité</th>
+                                    <th>Promo</th>
+                                    <th>% promo</th>
+                                    <th>Garantit</th>
+                                </tr>
+                                <?php
+                                require_once('C:/xampp/htdocs/distribpc/webadmin/modules/data_dashboard/nombre_composants/affiche_produit.php')
+                                ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+        </div>
+
+    </main>
+
+</section>
+<?php
+require_once('C:/xampp/htdocs/distribpc/webadmin/component/footer.php') ?>
