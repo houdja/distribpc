f490c481dcaf7feb88956476a2848db789a93c7e
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-28 12:22:49 +0400

    requete pour afficher les produits ordinateurs

---
+++ webadmin/modules/data_dashboard/nombre_ordinateurs/requete_produit.php
@@ -0,0 +1,21 @@
+<?php
+require_once('C:/xampp/htdocs/distribpc/webadmin/modules/data_dashboard/nombre_ordinateurs/compteur.php');
+
+$sql = "SELECT * FROM souscategorie as souscat JOIN produits as prod ON (prod.id_souscategorie = souscat.id_souscategorie) JOIN categorie as cat ON (prod.id_categorie = cat.id_categorie) WHERE cat.id_categorie = (SELECT id_categorie FROM categorie WHERE nom_categorie = 'Ordinateurs') LIMIT " . $premiereEntree . ", " . $messagesParPage . " ";
+
+try {
+
+    $stmt = $db->query($sql);
+
+    $count = $stmt->rowCount();
+    if ($count == 0) {
+
+        $msgprono = "En cours de saisie...";
+    }
+
+    if ($stmt === false) {
+        die("Erreur");
+    }
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
