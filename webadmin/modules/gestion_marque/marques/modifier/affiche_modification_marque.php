a2c2f24694dccd2b790aacc8d3674622edeed8ce
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-29 21:00:10 +0400

    ajout input type file

--- webadmin/modules/gestion_marque/marques/modifier/affiche_modification_marque.php
+++ webadmin/modules/gestion_marque/marques/modifier/affiche_modification_marque.php
@@ -9,3 +9,4 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC)
 
 <input type="hidden" name="id_marque" value="<?php echo htmlspecialchars($row['id_marque']); ?>">
 <input type="text" name="nom_marque" value="<?php echo htmlspecialchars($row['nom_marque']); ?>">
+<input type="file" name="image_marque" required>
