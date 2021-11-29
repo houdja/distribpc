cf73110f1e8e9eae3f14edc9cbd41b29525a1cab
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-29 20:27:54 +0400

    ajout de la foncttion trm pour eviter les espaces au debut et en fin de la valeur

--- webadmin/modules/produits/ajouter/traitement_ajout_produit.php
+++ webadmin/modules/produits/ajouter/traitement_ajout_produit.php
@@ -7,20 +7,20 @@ include("C:/xampp/htdocs/distribpc/webadmin/modules/connexiondb/connexiondb.php"
 <?php
 
 
-$categorie = $_POST['categorie'];
-$souscategorie = $_POST['souscategorie'];
-$marque = $_POST['marque'];
-$nom_produit = $_POST['nom_produit'];
-$prix = $_POST['prix'];
-$descr_c = $_POST['descr_c'];
-$descr_l = $_POST['descr_l'];
-$num_ref = $_POST['num_ref'];
-$dispo = $_POST['dispo'];
-$meilleur_v = $_POST['meilleur_v'];
-$promo = $_POST['promo'];
-$pourcent_promo = $_POST['pourcentage_promo'];
-$garantit = $_POST['garantit'];
-$image_produit = $_POST['image_produit'];
+$categorie = trim($_POST['categorie']);
+$souscategorie = trim($_POST['souscategorie']);
+$marque = trim($_POST['marque']);
+$nom_produit = trim($_POST['nom_produit']);
+$prix = trim($_POST['prix']);
+$descr_c = trim($_POST['descr_c']);
+$descr_l = trim($_POST['descr_l']);
+$num_ref = trim($_POST['num_ref']);
+$dispo = trim($_POST['dispo']);
+$meilleur_v = trim($_POST['meilleur_v']);
+$promo = trim($_POST['promo']);
+$pourcent_promo = trim($_POST['pourcentage_promo']);
+$garantit = trim($_POST['garantit']);
+$image_produit = trim($_POST['image_produit']);
 
 
 $sql = "INSERT INTO produits (`id_categorie`, `id_souscategorie`, `marque`, `nom_produit`, `prix`, `description_courte`, `description`, `num_reference`, `disponible`, `meilleur_vente`, `promo`, `pourcentage_promo`, `garantit`, `image_produit`)  VALUES (:categorie, :souscategorie, :marque, :nom_produit, :prix, :descr_c, :descr_l, :num_ref, :dispo, :meilleur_v, :promo, :pourcentage_promo, :garantit, :image_produit)";
@@ -60,5 +60,4 @@ try {
 <?php
 
 header('location:http://localhost/distribpc/webadmin/produit.php');
-
 ?>
