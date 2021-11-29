971a4b76b93ad835a3addb6c16f60fc91ab3329b
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-29 20:25:47 +0400

    traitement modif marque

---
+++ webadmin/modules/gestion_marque/marques/modifier/traitement_modification.php
@@ -0,0 +1,40 @@
+<?php
+
+include("C:/xampp/htdocs/distribpc/webadmin/modules/connexiondb/connexiondb.php");
+
+?>
+
+<?php
+
+$id_marque = $_POST['id_marque'];
+$marque = $_POST['nom_marque'];
+
+$sql = " UPDATE marque SET `nom_marque`=:marque WHERE `id_marque`=:id_marque ";
+
+try {
+
+  $stmt = $db->prepare($sql);
+  $stmt->bindValue('id_marque', $id_marque, PDO::PARAM_STR);
+  $stmt->bindValue('marque', $marque, PDO::PARAM_STR);
+  $stmt->execute();
+  if ($stmt == true) {
+
+    $msg = "Aucun résultat pour votre recherche";
+  }
+
+
+  if ($stmt === false) {
+    die("Erreur");
+  }
+} catch (PDOException $e) {
+}
+
+
+
+?>
+<?php
+
+header('location:http://localhost/distribpc/webadmin/marque.php');
+
+
+?>
