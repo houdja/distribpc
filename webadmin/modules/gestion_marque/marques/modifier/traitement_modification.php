8f16ff842d2f8b60779b6f3ead02181021e449fd
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-29 21:01:38 +0400

    traitement modification marque (modif)

--- webadmin/modules/gestion_marque/marques/modifier/traitement_modification.php
+++ webadmin/modules/gestion_marque/marques/modifier/traitement_modification.php
@@ -8,14 +8,16 @@ include("C:/xampp/htdocs/distribpc/webadmin/modules/connexiondb/connexiondb.php"
 
 $id_marque = $_POST['id_marque'];
 $marque = $_POST['nom_marque'];
+$imageMarque = $_POST['image_marque'];
 
-$sql = " UPDATE marque SET `nom_marque`=:marque WHERE `id_marque`=:id_marque ";
+$sql = " UPDATE marque SET `nom_marque`=:marque, `image_marque`=:image_marque WHERE `id_marque`=:id_marque ";
 
 try {
 
   $stmt = $db->prepare($sql);
   $stmt->bindValue('id_marque', $id_marque, PDO::PARAM_STR);
   $stmt->bindValue('marque', $marque, PDO::PARAM_STR);
+  $stmt->bindValue('image_marque', $imageMarque, PDO::PARAM_STR);
   $stmt->execute();
   if ($stmt == true) {
 
