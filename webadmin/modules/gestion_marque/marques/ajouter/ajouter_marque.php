97a2a241c52c0cec7f9b9bb5518cd5d99f870b8e
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-29 20:19:55 +0400

    creation page ajouter une marque

---
+++ webadmin/modules/gestion_marque/marques/ajouter/ajouter_marque.php
@@ -0,0 +1,34 @@
+<?php
+// session_start();
+// if(isset($_SESSION['sess_user_id']) && $_SESSION['sess_user_name'] != "") {
+
+// } else { 
+// 	header('location:index.php');
+// }
+?>
+<?php
+require_once('C:/xampp/htdocs/distribpc/webadmin/component/header.php');
+?>
+<section class="dashboard">
+  <?php
+  require_once('C:/xampp/htdocs/distribpc/webadmin/component/nav.php') ?>
+  <aside>
+    <?php
+    require_once('C:/xampp/htdocs/distribpc/webadmin/component/aside.php') ?>
+  </aside>
+  <main>
+    <div class='produit'>
+      <h1>Ajouter un produit</h1>
+      <form action="http://localhost/distribpc/webadmin/modules/gestion_marque/marques/ajouter/traitement_ajout_marque.php" method='POST' class='dash_form'>
+
+        <input type="text" name="marque" placeholder="Nom de la marque">
+
+        <button type="submit">Ajouter</button>
+
+      </form>
+    </div>
+  </main>
+</section>
+
+<?php
+require_once('C:/xampp/htdocs/distribpc/webadmin/component/footer.php') ?>
