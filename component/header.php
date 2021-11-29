7995c7121821f1b4f0cd84bfaec8f86e3aef49e8
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-29 20:13:57 +0400

    ajout de police

--- component/header.php
+++ component/header.php
@@ -16,6 +16,11 @@
 
     <!-- ICON -->
     <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">
+    
+    <!-- FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600&display=swap" rel="stylesheet">
 
     <title>DISTRIB PC</title>
 </head>
