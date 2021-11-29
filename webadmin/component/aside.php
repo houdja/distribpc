3c8805d16b5cf074094ac07a5c0b08cd2ac7d91d
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-29 20:19:02 +0400

    ajout lien vers marque.php

--- webadmin/component/aside.php
+++ webadmin/component/aside.php
@@ -3,7 +3,6 @@
 
     <li class=" <?php echo $_SERVER['REQUEST_URI'] == '/distribpc/webadmin/produit.php' ?  'active' : '';  ?>"><a href="http://localhost/distribpc/webadmin/produit.php"><img src="https://img.icons8.com/ios-glyphs/64/000000/activity-grid-2.png" /> Gestion produits</a></li>
 
-    <li><a href="#"><img src="https://img.icons8.com/ios-glyphs/64/000000/activity-grid-2.png" /> Gestion Categorie</a></li>
-    <li><a href="#"><img src="https://img.icons8.com/ios-glyphs/64/000000/activity-grid-2.png" /> Gestion SousCategorie</a></li>
+    <li><a href="http://localhost/distribpc/webadmin/marque.php"><img src="https://img.icons8.com/ios-glyphs/64/000000/activity-grid-2.png" />Gestion marques</a></li>
     <li><a href="#"><img src="https://img.icons8.com/small/64/000000/shutdown.png" />Déconnexion</a></li>
 </ul>
