de1ead56b0576dc0f37265881661847a85b8c0dc
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-30 14:37:32 +0400

    ajout lien logout

--- webadmin/component/aside.php
+++ webadmin/component/aside.php
@@ -3,6 +3,6 @@
 
     <li class=" <?php echo $_SERVER['REQUEST_URI'] == '/distribpc/webadmin/produit.php' ?  'active' : '';  ?>"><a href="http://localhost/distribpc/webadmin/produit.php"><img src="https://img.icons8.com/ios-glyphs/64/000000/activity-grid-2.png" /> Gestion produits</a></li>
 
-    <li><a href="http://localhost/distribpc/webadmin/marque.php"><img src="https://img.icons8.com/ios-glyphs/64/000000/activity-grid-2.png" />Gestion marques</a></li>
-    <li><a href="#"><img src="https://img.icons8.com/small/64/000000/shutdown.png" />Déconnexion</a></li>
+    <li class=" <?php echo $_SERVER['REQUEST_URI'] == '/distribpc/webadmin/marque.php' ?  'active' : '';  ?>"><a href="http://localhost/distribpc/webadmin/marque.php"><img src="https://img.icons8.com/ios-glyphs/64/000000/activity-grid-2.png" />Gestion marques</a></li>
+    <li><a href="logout.php"><img src="https://img.icons8.com/small/64/000000/shutdown.png" />Déconnexion</a></li>
 </ul>
