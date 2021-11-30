e8e89444718a28d3ed562a1078ad9b02415ae3af
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-30 10:43:12 +0400

    ajout partie verification img

--- webadmin/modules/gestion_marque/marques/modifier/traitement_modification.php
+++ webadmin/modules/gestion_marque/marques/modifier/traitement_modification.php
@@ -1,4 +1,55 @@
 <?php
+$target_dir = "C:/xampp/htdocs/distribpc/img/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+// Check if image file is a actual image or fake image
+if (isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if ($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if (
+  $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+  && $imageFileType != "gif"
+) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+  // if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+?>
+<?php
 
 include("C:/xampp/htdocs/distribpc/webadmin/modules/connexiondb/connexiondb.php");
 
@@ -8,7 +59,7 @@ include("C:/xampp/htdocs/distribpc/webadmin/modules/connexiondb/connexiondb.php"
 
 $id_marque = $_POST['id_marque'];
 $marque = $_POST['nom_marque'];
-$imageMarque = $_POST['image_marque'];
+$Nom_images = (basename($_FILES["fileToUpload"]["name"]));
 
 $sql = " UPDATE marque SET `nom_marque`=:marque, `image_marque`=:image_marque WHERE `id_marque`=:id_marque ";
 
@@ -17,7 +68,7 @@ try {
   $stmt = $db->prepare($sql);
   $stmt->bindValue('id_marque', $id_marque, PDO::PARAM_STR);
   $stmt->bindValue('marque', $marque, PDO::PARAM_STR);
-  $stmt->bindValue('image_marque', $imageMarque, PDO::PARAM_STR);
+  $stmt->bindValue('image_marque', $Nom_images, PDO::PARAM_STR);
   $stmt->execute();
   if ($stmt == true) {
 
@@ -36,7 +87,7 @@ try {
 ?>
 <?php
 
-header('location:http://localhost/distribpc/webadmin/marque.php');
+header('location:http://localhost/distribpc/webadmin/marque.php', 10);
 
 
 ?>
