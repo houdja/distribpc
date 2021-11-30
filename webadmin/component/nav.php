86956385b9d8690a2f4a9e115ff9f012e54577ec
houdja test <houdja-abdallahpro@hotmail.com>
2021-11-30 16:45:19 +0400

    modification de l'ordre

--- webadmin/component/nav.php
+++ webadmin/component/nav.php
@@ -1,9 +1,9 @@
 <nav>
     <a href="http://localhost/distribpc/webadmin/index.php" class="logo">Distrib pc</a>
     <div class="compte">
-        <a href="#"><img src="http://localhost/distribpc/webadmin/img/profile.jpg" alt="Profile picture"></a>
         <?php
-        echo '<h4>Bienvenue  ' . $_SESSION['sess_name'] . '</h4>';
+        echo '<h4>Bienvenue  '  . $_SESSION['sess_name'] . '</h4>';
         ?>
+        <a href="#"><img src="http://localhost/distribpc/webadmin/img/profile.jpg" alt="Profile picture"></a>
     </div>
 </nav>
